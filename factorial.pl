a407320fc8a299a3331f5de96bf0794433bea38c
Abhishek Dutta <35317861+Duttabhi@users.noreply.github.com>
2019-09-03 15:00:00 +0530

    updated comments

--- factorial.pl
+++ factorial.pl
@@ -8,6 +8,10 @@ pow(0, P, Ans) :- Ans is 0.
 pow(N, 0, Ans) :- Ans is 1.
 pow(N, P, Ans) :- N > 0, P > 0, Tmp is P-1, pow(N, Tmp, Var), Ans is (N * Var).
 
+% It goes on recursively calling the function till the base condition 
+% either Ans is 1 or 0. Now it will backtrack juct like stack "Var" will be first 0 or 1.
+% than it will go on updating Var and N * Var till we finally get the answer.
+
 
 
 
